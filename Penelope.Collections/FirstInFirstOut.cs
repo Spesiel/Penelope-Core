faf2f3daa6e568715be4200a3fa8f675b4bb66b2
Pénélope Blossom <luckypenny.blossom+github@gmail.com>
2016-02-07 20:44:44 -0800

    Initial commit

---
+++ Penelope.Collections/FirstInFirstOut.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Penelope.Collections
+{
+    public class FirstInFirstOut<T>
+    {
+        #region Fields + Properties
+
+        public int Count { get { return _Count; } }
+        public int PeakCount { get { return _PeakCount; } }
+        private T[] _Array;
+        private int _Count;
+        private int _CurrentIndex;
+        private int _GrowthRate;
+        private int _PeakCount;
+
+        #endregion Fields + Properties
+
+        #region To show the full queue
+
+        public ReadOnlyCollection<T> Queue
+        {
+            get
+            {
+                T[] result = new T[Count];
+                Array.Copy(_Array, _CurrentIndex, result, 0, Count);
+                return new ReadOnlyCollection<T>(result);
+            }
+        }
+
+        #endregion To show the full queue
+
+        #region Constructors
+
+        public FirstInFirstOut() : this(16, 4, null)
+        {
+        }
+
+        public FirstInFirstOut(int capacity) : this(capacity, 4, null)
+        {
+        }
+
+        public FirstInFirstOut(int capacity, int growthRate) : this(capacity, growthRate, null)
+        {
+        }
+
+        public FirstInFirstOut(ICollection<T> collection) : this(0, 4, collection)
+        {
+        }
+
+        public FirstInFirstOut(ICollection<T> collection, int growthRate) : this(0, growthRate, collection)
+        {
+        }
+
+        public FirstInFirstOut(int capacity, int growthRate, ICollection<T> collection)
+        {
+            if (collection != null) capacity =_Count = collection.Count;
+            if (capacity < 0) throw new ArgumentOutOfRangeException(nameof(capacity));
+            if (growthRate < 1) throw new ArgumentOutOfRangeException(nameof(growthRate));
+
+            _Array = new T[capacity];
+            _GrowthRate = growthRate;
+            _CurrentIndex = 0;
+
+            if (collection != null)
+            {
+                collection.CopyTo(_Array, 0);
+            }
+        }
+
+        #endregion Constructors
+
+        #region Queuing and Removing
+
+        public T Dequeue()
+        {
+            if (Count == 0)
+            {
+                return default(T);
+            }
+
+            T ans = _Array[_CurrentIndex++];
+            _Count--;
+
+            if (_Array.Length - (Count + _CurrentIndex) >= _GrowthRate + 2)
+            {
+                ResizeGrowOrShrink();
+            }
+
+            return ans;
+        }
+
+        public void Enqueue(T value)
+        {
+            if (_Array.Length - (Count + _CurrentIndex) <= _GrowthRate + 2)
+            {
+                ResizeGrowOrShrink();
+            }
+
+            _Array[_Count++] = value;
+            if (Count > PeakCount) _PeakCount = Count;
+        }
+
+        public bool Remove(T value)
+        {
+            bool ans = false;
+
+            int index = -1;
+            for (int i = 0; i < Count; i++)
+            {
+                if (_Array[i].Equals(value))
+                {
+                    index = i;
+                    ans = true;
+                    break;
+                }
+            }
+
+            Array.Copy(_Array, index + 1, _Array, index, (--_Count) - index);
+
+            return ans;
+        }
+
+        #endregion Queuing and Removing
+
+        #region Peeking
+
+        public T Peek()
+        {
+            return _Array[_CurrentIndex];
+        }
+
+        #endregion Peeking
+
+        #region SubArray
+
+        private T[] ResizeGrowOrShrink()
+        {
+            T[] result = new T[Count + _GrowthRate];
+            Array.Copy(_Array, _CurrentIndex, result, 0, Count);
+            _CurrentIndex = 0;
+            _Array = result;
+            return result;
+        }
+
+        #endregion SubArray
+    }
+}
---
+++ Penelope.CollectionsTest/FirstInFirstOutTest.cs
@@ -0,0 +1,219 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.ObjectModel;
+
+namespace Penelope.Collections.Test
+{
+    [TestClass]
+    public class FirstInFirstOutTest
+    {
+        #region Test Constructors
+
+        [TestMethod]
+        public void TestConstructor1()
+        {
+            FirstInFirstOut<int> fifo = new FirstInFirstOut<int>(3, 4, null);
+
+            Assert.AreEqual(0, fifo.Queue.Count);
+        }
+
+        [TestMethod]
+        public void TestConstructor2()
+        {
+            ReadOnlyCollection<int> expected = new ReadOnlyCollection<int>(new int[] { 1, 3, 5, 7, 9, 11 });
+            FirstInFirstOut<int> fifo = new FirstInFirstOut<int>(expected);
+
+            ReadOnlyCollection<int> actual = fifo.Queue;
+            Assert.AreEqual(expected.Count, actual.Count);
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+            Assert.AreEqual(expected[2], actual[2]);
+            Assert.AreEqual(expected[3], actual[3]);
+            Assert.AreEqual(expected[4], actual[4]);
+            Assert.AreEqual(expected[5], actual[5]);
+        }
+
+        #endregion Test Constructors
+
+        #region Tests Enqueue
+
+        [TestMethod]
+        public void TestEnqueue1()
+        {
+            FirstInFirstOut<int> fifo = new FirstInFirstOut<int>();
+            Assert.AreEqual(0, fifo.Queue.Count);
+
+            fifo.Enqueue(1);
+            fifo.Enqueue(3);
+            Assert.AreEqual(2, fifo.Queue.Count);
+
+            fifo.Enqueue(5);
+            Assert.AreEqual(3, fifo.Queue.Count);
+
+            fifo.Enqueue(7);
+            fifo.Enqueue(9);
+            fifo.Enqueue(11);
+
+            ReadOnlyCollection<int> expected = new ReadOnlyCollection<int>(new int[] { 1, 3, 5, 7, 9, 11 });
+            ReadOnlyCollection<int> actual = fifo.Queue;
+            Assert.AreEqual(expected.Count, actual.Count);
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+            Assert.AreEqual(expected[2], actual[2]);
+            Assert.AreEqual(expected[3], actual[3]);
+            Assert.AreEqual(expected[4], actual[4]);
+            Assert.AreEqual(expected[5], actual[5]);
+        }
+
+        [TestMethod]
+        public void TestEnqueue2()
+        {
+            FirstInFirstOut<int> fifo = new FirstInFirstOut<int>();
+
+            fifo.Enqueue(1);
+            fifo.Enqueue(3);
+            fifo.Enqueue(5);
+            fifo.Enqueue(7);
+            fifo.Enqueue(9);
+            fifo.Enqueue(11);
+
+            fifo.Remove(5);
+
+            ReadOnlyCollection<int> expected = new ReadOnlyCollection<int>(new int[] { 1, 3, 7, 9, 11 });
+            ReadOnlyCollection<int> actual = fifo.Queue;
+            Assert.AreEqual(expected.Count, actual.Count);
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+            Assert.AreEqual(expected[2], actual[2]);
+            Assert.AreEqual(expected[3], actual[3]);
+            Assert.AreEqual(expected[4], actual[4]);
+        }
+
+        [TestMethod]
+        public void TestEnqueue3()
+        {
+            FirstInFirstOut<int> fifo = new FirstInFirstOut<int>();
+
+            fifo.Enqueue(1);
+            fifo.Enqueue(3);
+            fifo.Enqueue(5);
+            fifo.Enqueue(7);
+            fifo.Enqueue(9);
+            fifo.Enqueue(11);
+
+            fifo.Remove(3);
+            fifo.Remove(5);
+            fifo.Remove(9);
+
+            ReadOnlyCollection<int> expected = new ReadOnlyCollection<int>(new int[] { 1, 7, 11 });
+            ReadOnlyCollection<int> actual = fifo.Queue;
+            Assert.AreEqual(expected.Count, actual.Count);
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+            Assert.AreEqual(expected[2], actual[2]);
+        }
+
+        [TestMethod]
+        public void TestEnqueue4()
+        {
+            FirstInFirstOut<int> fifo = new FirstInFirstOut<int>();
+
+            fifo.Enqueue(1);
+            fifo.Enqueue(3);
+            fifo.Enqueue(5);
+            fifo.Enqueue(7);
+            fifo.Enqueue(9);
+            fifo.Enqueue(11);
+
+            fifo.Remove(3);
+            fifo.Remove(5);
+            fifo.Remove(9);
+
+            fifo.Enqueue(6);
+
+            ReadOnlyCollection<int> expected = new ReadOnlyCollection<int>(new int[] { 1, 7, 11, 6 });
+            ReadOnlyCollection<int> actual = fifo.Queue;
+            Assert.AreEqual(expected.Count, actual.Count);
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+            Assert.AreEqual(expected[2], actual[2]);
+            Assert.AreEqual(expected[3], actual[3]);
+        }
+
+        [TestMethod]
+        public void TestEnqueue5()
+        {
+            FirstInFirstOut<int> fifo = new FirstInFirstOut<int>();
+
+            fifo.Enqueue(1);
+            fifo.Enqueue(3);
+            fifo.Enqueue(5);
+            fifo.Enqueue(7);
+            fifo.Enqueue(9);
+            fifo.Enqueue(11);
+
+            fifo.Remove(3);
+            fifo.Enqueue(10);
+            fifo.Remove(5);
+            fifo.Enqueue(6);
+            fifo.Remove(9);
+
+            ReadOnlyCollection<int> expected = new ReadOnlyCollection<int>(new int[] { 1, 7, 11, 6, 10 });
+            ReadOnlyCollection<int> actual = fifo.Queue;
+            Assert.AreEqual(expected.Count, actual.Count);
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+            Assert.AreEqual(expected[2], actual[2]);
+
+            Assert.AreNotEqual(expected[3], actual[3]);
+            Assert.AreNotEqual(expected[4], actual[4]);
+
+            Assert.AreEqual(expected[4], actual[3]);
+            Assert.AreEqual(expected[3], actual[4]);
+        }
+
+        #endregion Tests Enqueue
+
+        #region Tests Dequeue
+
+        [TestMethod]
+        public void TestDequeue1()
+        {
+            ReadOnlyCollection<int> expected = new ReadOnlyCollection<int>(new int[] { 1, 3, 5, 7, 9, 11 });
+            FirstInFirstOut<int> fifo = new FirstInFirstOut<int>(expected);
+
+            Assert.AreEqual(expected[0], fifo.Dequeue());
+            Assert.AreEqual(expected.Count - 1, fifo.Queue.Count);
+
+            Assert.AreEqual(expected[1], fifo.Dequeue());
+            Assert.AreEqual(expected[2], fifo.Dequeue());
+
+            Assert.AreEqual(expected[3], fifo.Peek());
+            Assert.AreEqual(expected[3], fifo.Dequeue());
+            Assert.AreEqual(expected[4], fifo.Peek());
+        }
+
+        [TestMethod]
+        public void TestDequeue2()
+        {
+            ReadOnlyCollection<int> expected = new ReadOnlyCollection<int>(new int[] { 1, 3, 5, 7, 9, 11 });
+            FirstInFirstOut<int> fifo = new FirstInFirstOut<int>(expected);
+
+            Assert.AreEqual(expected[0], fifo.Dequeue());
+            Assert.AreEqual(expected[1], fifo.Dequeue());
+            fifo.Enqueue(10);
+            Assert.AreEqual(expected[2], fifo.Dequeue());
+            fifo.Enqueue(6);
+            Assert.AreEqual(expected[3], fifo.Dequeue());
+
+            expected = new ReadOnlyCollection<int>(new int[] { 9, 11, 10, 6 });
+            ReadOnlyCollection<int> actual = fifo.Queue;
+            Assert.AreEqual(expected.Count, actual.Count);
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+            Assert.AreEqual(expected[2], actual[2]);
+            Assert.AreEqual(expected[3], actual[3]);
+        }
+
+        #endregion Tests Dequeue
+    }
+}
