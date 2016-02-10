37fcd6a1e6f903157e4eb7c9c26fb4f4f0faaa46
Pénélope Blossom <luckypenny.blossom+github@gmail.com>
2016-02-10 01:36:53 -0800

    Reusable collapsible component

---
+++ Penelope.Controls/Collapsible.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Penelope.Controls
+{
+    partial class Collapsible
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.control = new System.Windows.Forms.Control();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(0, 0);
+            this.label.Margin = new System.Windows.Forms.Padding(0);
+            this.label.MinimumSize = new System.Drawing.Size(100, 30);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(348, 30);
+            this.label.TabIndex = 0;
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // control
+            // 
+            this.control.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.control.Location = new System.Drawing.Point(0, 30);
+            this.control.Margin = new System.Windows.Forms.Padding(0);
+            this.control.Name = "control";
+            this.control.Size = new System.Drawing.Size(348, 318);
+            this.control.TabIndex = 1;
+            // 
+            // Collapsible
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.control);
+            this.Controls.Add(this.label);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "Collapsible";
+            this.Size = new System.Drawing.Size(348, 348);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Control control;
+    }
+}
---
+++ Penelope.Controls/Collapsible.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Penelope.Controls
+{
+    public partial class Collapsible : UserControl
+    {
+        #region Fields + Properties
+
+        public Control Control
+        {
+            get
+            {
+                return control;
+            }
+            set
+            {
+                control = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return label.Text;
+            }
+
+            set
+            {
+                label.Text = value;
+            }
+        }
+
+        private Size SizeCollapsed;
+        private Size SizeExpanded;
+
+        #endregion Fields + Properties
+
+        #region Constructors
+
+        public Collapsible()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public void SetCollapsingParameters(Size size, bool collapsedByDefault)
+        {
+            Size sizeCollapsed = SizeExpanded = size;
+            sizeCollapsed.Height = label.Height;
+            SizeCollapsed = sizeCollapsed;
+
+            // Requested collapsed by default
+            if (collapsedByDefault)
+            {
+                control.Visible = true;
+                label_DoubleClick(null, null);
+            }
+        }
+
+        private void label_DoubleClick(object sender, EventArgs e)
+        {
+            control.Visible = !control.Visible;
+
+            // Depending on visibility, the component is either collapsed or not
+            Size = control.Visible ? SizeExpanded : SizeCollapsed;
+        }
+
+        #endregion Methods
+    }
+}
